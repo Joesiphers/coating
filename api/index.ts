51d73496df0c7231af23af334e4fafba6205bea3
wenguang zhou <joesiphers@163.com>
2024-10-29 14:26:42 +1100

    new graphql base on wiordpress

---
+++ api/index.ts
@@ -0,0 +1,14 @@
+import {getProduct,getProductSummary,getProject} from './nextjsApi'
+import {getAllProducts_gql,getPreNextProduct_gql,getProductIdByTitle_gql,getAllProjects_gql,getProduct_gql,getProject_gql} from './wpApi'
+
+export {
+    getProduct,
+    getProductSummary,
+    getProject,
+    getAllProducts_gql,
+    getPreNextProduct_gql,
+    getProductIdByTitle_gql,
+    getAllProjects_gql,
+    getProduct_gql,
+    getProject_gql
+}
